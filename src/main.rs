205456a13945882b22f93b307975419eff3ccefe
Max Hackinger <mahaxi@gmail.com>
2020-04-06 23:50:43 -0400

    add 4. variable bindings, 4.1 mutability, 4.2 scope and shadowing

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,11 @@ fn main() {
     //enums();
     //enumUse();
     //clike();
-    //enumTestcase();
-    constants();
+    //enum_testcase();
+    //constants();
+    //varbind();
+    //mutability();
+    scope_shadow();
 }
 
 
@@ -716,7 +719,7 @@ impl ListNew {
     }
 }
 
-fn enumTestcase() {
+fn enum_testcase() {
     // Create an empty linked list
     let mut list = ListNew::new();
     
@@ -754,4 +757,73 @@ fn constants() {
     //THRESHOLD = 5;
 }
 
+/// 4. Veraible Bindings
+
+fn varbind() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+    
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+    
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+    
+    // The compiler warns about unused variable bindings; these warnings can
+    // be silenced by prefixing the variable name with an underscore
+    let _unused_veriable = 3u32;
+    
+    let _noisy_unused_variable = 2u32;
+}
+
+/// 4.1 Mutablility
+
+fn mutability() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+    
+    println!("Before mutation: {}", mutable_binding);
+    
+    // Ok
+    mutable_binding += 1;
+    
+    println!("After mutation: {}", mutable_binding);
+    
+    // Error
+    //_immutable_binding += 1;
+    
+}
+
+/// 4.2 scope and shadowing
+
+fn scope_shadow() {
+    // This binding lives in the main function
+    let long_lived_binding = 1;
+    
+    // This is a block, and has a smaller scope than the main function
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
+        
+        println!("inner short: {}", short_lived_binding);
+        
+        // This binding *shadows* the outer one
+        let long_lived_binding = 5_f32;
+        
+        println!("inner long: {}", long_lived_binding);
+    }
+    // End of the block
+    
+    // Error! `short_lived_binding` doesn't exist in this scope
+    //println!("outer short: {}", short_lived_binding);
+    
+    println!("outer long: {}", long_lived_binding);
+    
+    // This binding also *shadows* the previous binding
+    let long_lived_binding = 'a';
+    
+    println!("outer long: {}", long_lived_binding);
+}
 
