f251a49c5b7a40017cad38a204472f6a3d85f916
Max Hackinger <mahaxi@gmail.com>
2020-04-09 01:38:15 -0400

    add all of chapter 5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(overflowing_literals)]
+
 // here i'll add some comments not much changed here
 
 /* here is a block comment, wow this is crAZY fun
@@ -25,7 +27,11 @@ fn main() {
     //varbind();
     //mutability();
     //scope_shadow();
-    declare_first();
+    //declare_first();
+    //casting();
+    //literals();
+    //inference();
+    aliasing();
 }
 
 
@@ -850,5 +856,112 @@ fn declare_first() {
     another_binding = 1;
     
     println!("another_binding: {}", another_binding);
+}
+
+
+
+/// 5. Types, 5.1 Casting
+
+// Suppress all wanings from casts which overflow.
+
+
+fn casting() {
+    let decimal = 65.4321_f32;
+    
+    // Error! No implicit conversion
+    //let integer: u8 = decimal;
+    
+    // Explicit conversion
+    let integer = decimal as u8;
+    let character = integer as char;
+    
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+    
+    // when casting any value to an unsigned type, T,
+    // std::T::Max + 1 is addedd or subtracted until the value
+    // fits into the new type
+    
+    // 1000 already fits in a u16
+    println!("1000 as a u16 is: {}", 1000 as u16);
+    
+    // 1000 - 256 - 256 -256 = 232
+    // under the hood, the first 8 least significant bits (LSB) are kept,
+    // while the rest towards the most significant bit (MSB) get truncated.
+    println!("1000 as a u8 is: {}", 1000 as u8);
+    // -1 + 256 = 255
+    println!(" -1 as a u8 is : {}", (-1i8) as u8);
+    
+    // For positive numbers, this is the same as the modulus
+    println!("1000 mod 256 is : {}", 1000 % 256);
+    
+    // When casting to a signed type, the (bitwise) result is the same as
+    // first casting to the corresponding unsigned type. If the most significant
+    // bit of that value is 1, then the value is negative.
+    
+    // Unless it already fits, of course.
+    println!(" 128 as a i16 is: {}", 128 as i16);
+    // 128 as u8 -> 128, whose two's complement in eight bits is:
+    println!(" 128 as a i8 is: {}", 128 as i8);
+    
+    // repeating the example above
+    // 1000 as u8 -> 232
+    println!("1000 as a u8 is : {}", 1000 as u8);
+    // and the two's complement of 232 is -24
+    println!(" 232 as a i8 is : {}", 232 as i8);
+}
+
+/// 5.2 Literals
+
+fn literals() {
+    // Suffixed literals, their types are known at initialization
+    let x = 1u8;
+    let y = 2u32;
+    let z = 3f32;
+    
+    // Unsuffixed literal, their types depend on how they are used
+    let i = 1;
+    let f = 1.0;
+    
+    // `size_of_val` returns the size of a variable in bytes
+    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+}
+
+/// 5.3 Inference
+
+fn inference() {
+    // Because of the annotation, the compiler knows that `elem` has type u8.
+    let elem = 5u8;
+    
+    // Create an empty vector (a growable array).
+    let mut vec = Vec::new();
+    // At this point the compiler doesn't know the exact type of `vec`, it
+    // just knows that it's a vector of something (`Vec<_>`).
+    
+    // Insert `elem` in the vector.
+    vec.push(elem);
+    // Aha! Now the compiler knows that `vec` is a vector of `u8`s (`Vec<u8>`)
+    println!("{:?}", vec);
+}
+
+/// 5.4 Aliasing
+
+// `NanoSecond` is a new name for u`u64`.
+type NanoSecond = u64;
+type Inch = u64;
+
+fn aliasing() {
+    let nanoseconds: NanoSecond = 5;
+    let inches: Inch = 2;
+    
+    // Note that type aliases *don't* provide any extra type safety, because
+    // aliases are *not* new types
+    println!("{} nanoseconds + {} inches = {} unit?",
+        nanoseconds,
+        inches,
+        nanoseconds + inches);
 }
 
