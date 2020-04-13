2cc07976675253bd271539c6370d07658606741e
Max Hackinger <mahaxi@gmail.com>
2020-04-13 15:16:45 -0400

    add 8.2 loop to flow_control function

--- src/main.rs
+++ src/main.rs
@@ -1081,5 +1081,30 @@ fn flow_control() {
         };
         
     println!("{} -> {}", n, big_n);
+    
+    let mut count = 0u32;
+    
+    println!("Let's count until infinity!");
+    
+    // Infinite loop
+    loop {
+        count += 1;
+        
+        if count == 3 {
+            println!("three");
+            
+            // Skip the rest of this iteration
+            continue;
+        }
+        
+        println!("{}", count);
+        
+        if count == 5 {
+            println!("OK, that's enough");
+            
+            // Exit this loop
+            break;
+        }
+    }
 }
 
