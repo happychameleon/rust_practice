2ff34db13f2fb5ffebd7ecd287e8c098e941e602
Max Hackinger <mahaxi@gmail.com>
2020-04-17 18:13:39 -0400

    add match tuple and correct typo

--- src/main.rs
+++ src/main.rs
@@ -1221,7 +1221,7 @@ fn flow_control() {
         // Match a single value
         1 => println!("One!"),
         // Mach several values
-        2 | 3 | 5 | 7 | 11 => println!("Thsi is a prime") ,
+        2 | 3 | 5 | 7 | 11 => println!("This is a prime") ,
         // Match an inclusive range
         13..=19 => println!("A teen"),
         // Handle the rest of cases
@@ -1238,5 +1238,17 @@ fn flow_control() {
     
     println!("{} -> {}", boolean, binary);
     
+    // tuple destructur with match
+    let pair = (3, 19);
+    
+    println!("Tell me about {:?}", pair);
+    // Match can be used to destructure a tuple
+    match pair {
+        // Destructure the second
+        (0, y) => println!("First is `0` and `y` is `{:?}`", y),
+        (x, 0) => println!("`x` is `{:?}` and last is `0`", x),
+        _      => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }    
 }
 
