fe6db66257719ee24ad9e7ef0d79c1e1beed816f
Max Hackinger <mahaxi@gmail.com>
2020-05-04 18:34:39 -0400

    add 9.3 highter order functions

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,8 @@ fn main() {
     //flow_control();
     //functioned_fizzbuzz();
     //methods();
-    closures();
+    //closures();
+    hof();
 }
 
 
@@ -1921,4 +1922,42 @@ fn closures() {
     let index_of_first_negative_number = vec.iter().position(|x| x < &0);
     assert_eq!(index_of_first_negative_number, None);
 }
+
+/// 9.3 higher order functions
+
+fn is_odd(n: u32) -> bool {
+    n % 2 == 1
+}
+
+fn hof() {
+    println!("Find the sum of all the squared odd numbers under 1000");
+    let upper = 1000;
+    
+    // Imperative approach
+    // Declare accumulator variable
+    let mut acc = 0;
+    // Iterate: 0, 1, 2, ... to infinity
+    for n in 0.. {
+        // Square the number
+        let n_squared = n * n;
+        
+        if n_squared >= upper {
+            // Break loop if exceeded the upper limit
+            break;
+        } else if is_odd(n_squared) {
+             // Accumulate value, if it's odd
+             acc += n_squared;
+        }
+    }
+    println!("imperative style: {}", acc);
+    
+    // Functional approach
+    let sum_of_squared_odd_numbers: u32 = 
+        (0..).map(|n| n * n)				 // All natural numbers squared
+            .take_while(|&n_squared| n_squared < upper) // Below upper limit
+            .filter(|&n_squared| is_odd(n_squared))	 // That are odd
+            .fold(0, |acc, n_squared| acc + n_squared); // Sum them
+    println!("functional style: {}", sum_of_squared_odd_numbers);
+}
+
 
