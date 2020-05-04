1ed38bbae380470e78531c874d1d12d42c020449
Max Hackinger <mahaxi@gmail.com>
2020-05-03 20:43:56 -0400

    add example 9.2.6.1 to closures function

--- src/main.rs
+++ src/main.rs
@@ -1868,5 +1868,23 @@ fn closures() {
     fn_plain();
     fn_mut();
     fn_once();
+    
+    // 9.2.6.1 iterator::any
+    
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    
+    // `iter()` for vecs yields `&i32`. Destructure to `i32`.
+    println!("2 in vec1: {}", vec1.iter().any(|&x| x == 2));
+    // `into_iter()` for vecs yields `i32`. No destructuring required.
+    println!("2 in vec2: {}", vec2.into_iter().any(|x| x == 2));
+    
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+    
+    // `iter()` for arrays yields `&i32`.
+    println!("2 in array1: {}", array1.iter().any(|&x| x == 2));
+    // `into_iter()` for arrays unusually yields `&i32`.
+    println!("2 in array2: {}", array2.into_iter().any(|&x| x == 2));
 }
 
