0ba8b97f79664bbdf148d8b35fdb8fdddb7c4f81
Max Hackinger <mahaxi@gmail.com>
2020-05-05 16:58:17 -0400

    add diverging functions example

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,8 @@ fn main() {
     //functioned_fizzbuzz();
     //methods();
     //closures();
-    hof();
+    //hof();
+    diverg_func();
 }
 
 
@@ -1960,4 +1961,26 @@ fn hof() {
     println!("functional style: {}", sum_of_squared_odd_numbers);
 }
 
+/// 9.4 Diverging functions
+
+fn diverg_func() {
+     fn sum_odd_numbers(up_to: u32) -> u32 {
+         let mut acc = 0;
+         for i in 0..up_to {
+              // Notice that the return type of this match expression must be u32
+              // becasue of the type of the "addition" variable.
+              let addition: u32 = match i%2 == 1 {
+                  // The "i" variable is of type u32, which is perfectly fine.
+                  true => i,
+                  // On the otherr hand, the "continue" expression does not return
+                  // u32, but it is still fine, because it never returns and therefore
+                  // does not violate the type requirementsof the match expression.
+                  false => continue,
+              };
+              acc += addition;
+         }
+         acc
+     }
+     println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
+}
 
